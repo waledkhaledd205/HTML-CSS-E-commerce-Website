4fd3ddbaea7c34f17837e102b40ff9de6805b6f8
unknown <waledkhaledd205@gmail.com>
2025-03-20 20:27:06 +0200

    first commit

---
+++ Js/main.js
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 100) {
+        headerElement.classList.add("scroll-down")
+    } else {
+        headerElement.classList.remove("scroll-down")
+    }
+})
